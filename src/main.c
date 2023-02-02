76c59a47c738bf3d454f8f0f86585ec1e947ade2
rwn42 <rwn757@gmail.com>
2023-02-01 16:32:22 -0800

    CLI is ready to start on compiler

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "include/parser.h"
 #include "include/interpreter.h"
 
+void interpret(ProgramData pd);
 
 int main(int argc, char **argv){
     if(argc <= 1)
@@ -13,16 +15,33 @@ int main(int argc, char **argv){
 
     ProgramData pd = BfInstructionsFromFile(argv[1]);
 
+    char* mode = "compile";
+    if(argc >= 3){
+        const char* subcommand = argv[2];
+        if(strcmp(subcommand, "run") == 0)
+        {
+            mode = "run";
+        }
+        else if(strcmp(subcommand, "compile") == 0);
+        else printf("ERROR: Unknown Subcommand %s", subcommand);
+    }
+
+    if(strcmp(mode, "run") == 0) interpret(pd);
+
+
+}
+
+void interpret(ProgramData pd)
+{
     Bvm m = {.program_length=pd.length, .program=pd.program, .ip=0, .head=0, .tape={0}};
 
     while(1){
         BvmError result = BvmExecute(&m);
-        if(result == ERR_HALT) return 0;
+        if(result == ERR_HALT) return;
         if(result != ERR_NO_ERROR){
             printf("ERROR: %s\n", BvmStrError(result));
             exit(1);
         }
     }
     BvmDestroy(&m);
-
 }
